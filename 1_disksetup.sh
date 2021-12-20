6d32897de62df9a8e8c0b83298ee0512567f4d70
Sri Harsha <kudoark@gmail.com>
2021-12-20 23:52:10 +0530

    fixed swap size error

--- 1_disksetup.sh
+++ 1_disksetup.sh
@@ -84,7 +84,7 @@ read swapsize
 #Put swap into the actual system, not into RAM disk, otherwise there is no point in it, it'll cache RAM into RAM. So, /mnt/ everything.
 mkdir /mnt/opt/swap #make a dir that we can apply NOCOW to to make it btrfs-friendly.
 chattr +C /mnt/opt/swap #apply NOCOW, btrfs needs that.
-dd if=/dev/zero of=/mnt/opt/swap/swapfile bs=1M count=15360 status=progress
+dd if=/dev/zero of=/mnt/opt/swap/swapfile bs=1M count=$swapsize status=progress
 chmod 600 /mnt/opt/swap/swapfile #set permissions.
 chown root /mnt/opt/swap/swapfile
 mkswap /mnt/opt/swap/swapfile
