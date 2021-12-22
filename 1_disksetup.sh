8049a2e54eedab074603d4151e3ca2a6f0a47347
Sri Harsha <kudoark@gmail.com>
2021-12-22 19:21:20 +0530

    swap file errors with btrfs snapshot

--- 1_disksetup.sh
+++ 1_disksetup.sh
@@ -101,21 +101,21 @@ echo "DISK=${DISK}" >> ${HOME}/ArchBaseInstall/install.conf
 cp -R ${SCRIPT_DIR} /mnt/root/ArchBaseInstall
 cp /etc/pacman.d/mirrorlist /mnt/etc/pacman.d/mirrorlist
 
-echo "--------------------------------------"
-echo "-- Making swap file --"
-echo "--------------------------------------"
-echo "****Please enter the size(MB) of SWAP file****"
-read swapsize
-#Put swap into the actual system, not into RAM disk, otherwise there is no point in it, it'll cache RAM into RAM. So, /mnt/ everything.
-mkdir /mnt/opt/swap #make a dir that we can apply NOCOW to to make it btrfs-friendly.
-chattr +C /mnt/opt/swap #apply NOCOW, btrfs needs that.
-dd if=/dev/zero of=/mnt/opt/swap/swapfile bs=1M count=$swapsize status=progress
-chmod 600 /mnt/opt/swap/swapfile #set permissions.
-chown root /mnt/opt/swap/swapfile
-mkswap /mnt/opt/swap/swapfile
-swapon /mnt/opt/swap/swapfile
-#The line below is written to /mnt/ but doesn't contain /mnt/, since it's just / for the sysytem itself.
-echo "/opt/swap/swapfile	none	swap	sw	0	0" >> /mnt/etc/fstab #Add swap to fstab, so it KEEPS working after installation.
+#echo "--------------------------------------"
+#echo "-- Making swap file doesnot work with snapshots --"
+#echo "--------------------------------------"
+#echo "****Please enter the size(MB) of SWAP file****"
+#read swapsize
+##Put swap into the actual system, not into RAM disk, otherwise there is no point in it, it'll cache RAM into RAM. So, /mnt/ everything.
+#mkdir /mnt/opt/swap #make a dir that we can apply NOCOW to to make it btrfs-friendly.
+#chattr +C /mnt/opt/swap #apply NOCOW, btrfs needs that.
+#dd if=/dev/zero of=/mnt/opt/swap/swapfile bs=1M count=$swapsize status=progress
+#chmod 600 /mnt/opt/swap/swapfile #set permissions.
+#chown root /mnt/opt/swap/swapfile
+#mkswap /mnt/opt/swap/swapfile
+#swapon /mnt/opt/swap/swapfile
+##The line below is written to /mnt/ but doesn't contain /mnt/, since it's just / for the sysytem itself.
+#echo "/opt/swap/swapfile	none	swap	sw	0	0" >> /mnt/etc/fstab #Add swap to fstab, so it KEEPS working after installation.
 
 
 echo "--------------------------------------"
