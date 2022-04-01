c18517719dce73b6c2d243275e9bb1e5cc525c9a
sri harsha <kudoark@gmail.com>
2022-04-01 19:10:06 +0000

    fixed permissions and added intel gpu tools

--- 2_setup.sh
+++ 2_setup.sh
@@ -146,7 +146,7 @@ if lspci | grep -E "NVIDIA|GeForce"; then
 elif lspci | grep -E "Radeon"; then
     pacman -S xf86-video-amdgpu --noconfirm --needed
 elif lspci | grep -E "Integrated Graphics Controller"; then
-    pacman -S libva-intel-driver libvdpau-va-gl lib32-vulkan-intel vulkan-intel libva-intel-driver libva-utils --needed --noconfirm
+    pacman -S libva-intel-driver libvdpau-va-gl lib32-vulkan-intel vulkan-intel libva-intel-driver libva-utils intel-gpu-tools --needed --noconfirm
 fi
 
 
