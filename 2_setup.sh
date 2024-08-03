4d995bbd1cf81710090fbd3314f955a6e7b2670a
sriharsha <kudoark@gmail.com>
2024-08-03 03:03:37 +0000

    dhcpcd service does not exist anymore

--- 2_setup.sh
+++ 2_setup.sh
@@ -255,8 +255,6 @@ echo -e "\nEnabling essential services"
 systemctl enable cups.service
 ntpd -qg
 systemctl enable ntpd.service
-systemctl disable dhcpcd.service
-systemctl stop dhcpcd.service
 systemctl enable NetworkManager.service
 systemctl enable bluetooth
 echo "-------------------------------------------------"
