c85a1aa2bc6cddda1a691bc630d4d6bb1c3fbe01
Harsha <kudoark@gmail.com>
2026-09-15 13:49:20 +0530

    updates to packages. nvidia and radeon driver installs

--- 2_setup.sh
+++ 2_setup.sh
@@ -9,7 +9,12 @@
 # ░░█████████  █████     ░░████████ ██████ ░░██████ ░░████████░░██████ 
 #  ░░░░░░░░░  ░░░░░       ░░░░░░░░ ░░░░░░   ░░░░░░   ░░░░░░░░  ░░░░░░  
 #-------------------------------------------------------------------------
-source /mnt/root/ArchBaseInstall/install.conf
+source /root/ArchBaseInstall/install.conf
+if [[ ${DISK} =~ "nvme" ]]; then
+    ROOTPART="${DISK}p3"
+else
+    ROOTPART="${DISK}3"
+fi
 echo "--------------------------------------"
 echo "--          Network Setup           --"
 echo "--------------------------------------"
@@ -38,13 +43,10 @@ echo "       Setup Language to US and set locale       "
 echo "-------------------------------------------------"
 sed -i 's/^#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/' /etc/locale.gen
 locale-gen
-timedatectl set-timezone Asia/Kolkata
-timedatectl set-ntp 1
-localectl set-locale LANG="en_US.UTF-8" LC_TIME="en_US.UTF-8"
+ln -sf /usr/share/zoneinfo/Asia/Kolkata /etc/localtime
 hwclock --systohc --utc
-# Add sudo no password rights
-sed -i 's/^# %wheel ALL=(ALL:ALL) NOPASSWD: ALL/%wheel ALL=(ALL:ALL) NOPASSWD: ALL/' /etc/sudoers
-
+echo "LANG=en_US.UTF-8" > /etc/locale.conf
+echo "LC_TIME=en_US.UTF-8" >> /etc/locale.conf
 #Add parallel downloading
 sed -i 's/^#Para/Para/' /etc/pacman.conf
 
@@ -71,7 +73,7 @@ PKGS=(
 'cronie'
 'cups'
 'dialog'
-'exfat-utils'
+'exfatprogs'
 'extra-cmake-modules'
 'git'
 'gptfdisk'
@@ -83,11 +85,12 @@ PKGS=(
 'linux-headers'
 'make'
 'nano'
+'fastfetch'
 'neofetch'
 'networkmanager'
 'ntp'
 'openssh'
-'p7zip'
+'7zip'
 'pacman-contrib'
 'rsync'
 'sudo'
@@ -104,8 +107,11 @@ PKGS=(
 #installing additional packages
 for PKG in "${PKGS[@]}"; do
     echo "INSTALLING: ${PKG}"
-    sudo pacman -S "$PKG" --noconfirm --needed
+    pacman -S "$PKG" --noconfirm --needed
 done
+
+# Add sudo no password rights
+sed -i 's/^# %wheel ALL=(ALL:ALL) NOPASSWD: ALL/%wheel ALL=(ALL:ALL) NOPASSWD: ALL/' /etc/sudoers
 
 echo "-------------------------------------------------"
 echo "          Installing CPU & GPU Drivers           "
@@ -126,26 +132,26 @@ else
     echo "CPU not recognized"
 	echo "Which microcode to install (Intel/AMD/ignore)"
 	read proctype
-	if [[$proctype =~ "Intel"]]; then
+	if [[ $proctype =~ "Intel" ]]; then
 		echo "Installing Intel microcode"
 		pacman -S  intel-ucode --noconfirm
 		proc_ucode=intel-ucode.img
-	elif [[$proctype =~ "AMD"]]; then
+	elif [[ $proctype =~ "AMD" ]]; then
     	echo "Installing AMD microcode"
 		pacman -S  amd-ucode --noconfirm
 		proc_ucode=amd-ucode.img
-	elif [[$proctype =~ "ignore"]]; then
+	elif [[ $proctype =~ "ignore" ]]; then
 		echo "User chose not to install microcode"
 	fi
 fi
 
 # Graphics Drivers find and install
-if lspci | grep -E "NVIDIA|GeForce"; then
-    pacman -S nvidia-dkms nvidia-utils opencl-nvidia libglvnd lib32-libglvnd lib32-nvidia-utils lib32-opencl-nvidia nvidia-settings --noconfirm --needed	
-elif lspci | grep -E "Radeon"; then
+if lspci | grep -E "VGA|3D" | grep -E "NVIDIA|GeForce"; then
+    pacman -S nvidia-dkms nvidia-utils libglvnd lib32-libglvnd lib32-nvidia-utils nvidia-settings --noconfirm --needed	
+elif lspci | grep -E "VGA|3D" | grep -E "Radeon"; then
     pacman -S xf86-video-amdgpu --noconfirm --needed
-elif lspci | grep -E "Integrated Graphics Controller"; then
-    pacman -S libva-intel-driver libvdpau-va-gl lib32-vulkan-intel vulkan-intel libva-intel-driver libva-utils intel-gpu-tools --needed --noconfirm
+elif lspci | grep -E "VGA|3D" | grep -E "Intel"; then
+    pacman -S libva-intel-driver intel-media-driver libvdpau-va-gl lib32-vulkan-intel vulkan-intel libva-utils intel-gpu-tools --needed --noconfirm
 fi
 
 
@@ -164,7 +170,7 @@ then
 127.0.0.1	localhost
 ::1		localhost
 EOL
-echo "${nameofmachine}.localdomain	${nameofmachine}" >> /etc/hosts
+echo "127.0.1.1	${nameofmachine}.localdomain	${nameofmachine}" >> /etc/hosts
 
 else
 	echo "You are already a user proceed with aur installs"
@@ -185,32 +191,34 @@ if [[ ${bootloader} =~ "grub"  ]]; then
 echo "--------------------------------------"
 echo "-- GRUB EFI Bootloader Install&Check--"
 echo "--------------------------------------"
-pacman -S grub grub-customizer grub-btrfs os-prober mtools dosfstools efibootmgr --noconfirm --needed
+pacman -S grub grub-btrfs os-prober mtools dosfstools efibootmgr --noconfirm --needed
 
 grub-install --efi-directory=/boot ${DISK}
 
 grub-mkconfig -o /boot/grub/grub.cfg
 
-if lspci | grep -E "NVIDIA|GeForce"; then
-sed -i "s/modules=()/modules=( nvidia nvidia_modeset nvidia_uvm nvidia_drm)/" /etc/mkinitcpio.conf   
+if lspci | grep -E "VGA|3D" | grep -E "NVIDIA|GeForce"; then
+sed -i 's/^MODULES=(\([^)]*\))/MODULES=(\1 nvidia nvidia_modeset nvidia_uvm nvidia_drm)/' /etc/mkinitcpio.conf   
 [ ! -d "/etc/pacman.d/hooks" ] && mkdir -p /etc/pacman.d/hooks
 
-cat > /etc/pacman.d/hooks/nvidia <<EOL
+cat > /etc/pacman.d/hooks/nvidia.hook <<EOL
 [Trigger]
 Operation=Install
 Operation=Upgrade
 Operation=Remove
 Type=Package
-Target=nvidia
+Target=nvidia-dkms
 
 [Action]
+Description=Update initramfs after nvidia driver changes
 Depends=mkinitcpio
 When=PostTransaction
 Exec=/usr/bin/mkinitcpio -P
 EOL
+mkinitcpio -P
 fi
-if lspci | grep -E "Radeon"; then
-    sed -i "s/modules=()/modules=(amdgpu radeon)/" /etc/mkinitcpio.conf 
+if lspci | grep -E "VGA|3D" | grep -E "Radeon"; then
+    sed -i 's/^MODULES=(\([^)]*\))/MODULES=(\1 amdgpu radeon)/' /etc/mkinitcpio.conf 
 	mkinitcpio -P  
 fi
 echo "--------------------------------------"
@@ -227,25 +235,30 @@ title Crusedo Linux
 linux /vmlinuz-linux  
 initrd  /initramfs-linux.img
 EOL
-if lspci | grep -E "NVIDIA|GeForce"; then
-sed -i "s/modules=()/modules=( nvidia nvidia_modeset nvidia_uvm nvidia_drm)/" /etc/mkinitcpio.conf
-echo "options root=PARTUUID=$(blkid -s PARTUUID -o value ${DISK}2) rw rootflags=subvol=@ nvidia-drm.modeset=1" >> /boot/loader/entries/crusedoarch.conf
+if [[ -n "${proc_ucode}" ]]; then
+    sed -i "s|^initrd  /initramfs-linux.img|initrd /${proc_ucode}\ninitrd  /initramfs-linux.img|" /boot/loader/entries/crusedoarch.conf
+fi
+if lspci | grep -E "VGA|3D" | grep -E "NVIDIA|GeForce"; then
+sed -i 's/^MODULES=(\([^)]*\))/MODULES=(\1 nvidia nvidia_modeset nvidia_uvm nvidia_drm)/' /etc/mkinitcpio.conf
+echo "options root=PARTUUID=$(blkid -s PARTUUID -o value ${ROOTPART}) rw rootflags=subvol=@ nvidia-drm.modeset=1" >> /boot/loader/entries/crusedoarch.conf
 [ ! -d "/etc/pacman.d/hooks" ] && mkdir -p /etc/pacman.d/hooks
-cat > /etc/pacman.d/hooks/nvidia <<EOL
+cat > /etc/pacman.d/hooks/nvidia.hook <<EOL
 [Trigger]
 Operation=Install
 Operation=Upgrade
 Operation=Remove
 Type=Package
-Target=nvidia
+Target=nvidia-dkms
 
 [Action]
+Description=Update initramfs after nvidia driver changes
 Depends=mkinitcpio
 When=PostTransaction
 Exec=/usr/bin/mkinitcpio -P
 EOL
+mkinitcpio -P
 else
-echo "options root=PARTUUID=$(blkid -s PARTUUID -o value ${DISK}2) rw rootflags=subvol=@" >> /boot/loader/entries/crusedoarch.conf
+echo "options root=PARTUUID=$(blkid -s PARTUUID -o value ${ROOTPART}) rw rootflags=subvol=@" >> /boot/loader/entries/crusedoarch.conf
 fi
 
 fi
